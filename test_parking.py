ca2447392fd87a6efd812ff1553196657ece1463
Hamza-meskini <meskinihamza31@gmail.com>
2025-12-27 02:13:00 +0100

    last commit 2

--- test_parking.py
+++ test_parking.py
@@ -10,7 +10,9 @@ def test_entree_standard():
     p.gerer_entree(est_abonne=False)
     assert p.places_libres == 4
     # Vérifie que l'automate est bien revenu à un état d'accueil (STATIONNEMENT ou DISPO)
-    assert p.automate.etat_courant.id_etat in [0, 4] 
+    # Note: Dans la nouvelle logique, s'il reste de la place, il revient à DISPONIBLE pour le prochain
+    # Sauf s'il est au milieu d'une transition, mais ici gerer_entree est synchrone (sans callback)
+    assert p.automate.etat_courant.label_etat in ["DISPONIBLE", "STATIONNEMENT"]
 
 def test_saturation_parking():
     # Petit parking de 1 place
@@ -24,17 +26,20 @@ def test_saturation_parking():
 
 def test_gratuité_abonne():
     p = ParkingSystem(places_totales=5)
-    p.gerer_entree() # On gare une voiture
+    p.gerer_entree(est_abonne=True) # On gare une voiture
     
     # On la fait sortir en tant qu'abonné
     solde_avant = p.recettes_totales
-    p.gerer_sortie(est_abonne=True)
+    p.gerer_sortie(est_abonne=True, montant=0.0)
     assert p.recettes_totales == solde_avant # Pas de changement de caisse
 
 def test_paiement_visiteur():
     p = ParkingSystem(places_totales=5)
-    p.gerer_entree()
+    p.gerer_entree(est_abonne=False)
     
     solde_avant = p.recettes_totales
-    p.gerer_sortie(est_abonne=False)
-    assert p.recettes_totales == solde_avant + 15.0 # +15 DH
+    # Simulation d'un montant calculé dynamique (ex: 20.0)
+    prix_test = 20.0
+    p.gerer_sortie(est_abonne=False, montant=prix_test)
+    
+    assert p.recettes_totales == solde_avant + prix_test
